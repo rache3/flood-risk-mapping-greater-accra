4301feee305970cf5c0c0083a27e015a08995393
rachelGithub-1 <118177691+rachelGithub-1@users.noreply.github.com>
2026-08-04 18:42:58 +0000

    migrate storage from GCS to Cloudflare R2 - complete GCP exit

--- docs/script.js
+++ docs/script.js
@@ -1,6 +1,6 @@
 const TITILER_URL = "https://titiler-z2qegb4nha-uc.a.run.app";
-const R2_PUBLIC = "https://storage.googleapis.com/accra-flood-risk";
-const COG_URL = `${R2_PUBLIC}/rasters/flood_risk_map.cog.tif?v=11`;
+const R2_PUBLIC = "https://pub-8eb5bbc88b134fe795056f57988e73e4.r2.dev";
+const COG_URL = `${R2_PUBLIC}/rasters/flood_risk_map.cog.tif?v=12`;
 const GEOJSON_URL = "./gadm41_GHA_accra.json";
 
 const TILE_URL = `${TITILER_URL}/cog/tiles/{z}/{x}/{y}` +
